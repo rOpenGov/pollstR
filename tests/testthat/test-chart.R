d70f5d2c7f3d2a604bcb451bf598144b37e66f85
Jeff Arnold <jeffrey.arnold@gmail.com>
2014-04-17 20:02:02 -0400

    added testthat tests

---
+++ tests/testthat/test-chart.R
@@ -0,0 +1,39 @@
+context("chart")
+
+slug <- "obama-job-approval"
+chartu <- pollster_chart(slug, convert = FALSE)
+
+test_that("API returns data", {
+    expect_true(length(chartu) > 0)    
+})
+
+chart <- pollster_chart(slug)
+
+test_that("chart data is in the correct format", {
+    expect_is(chart, "list")
+    expect_equal(length(chart), 10)
+    expect_equal(names(chart),
+                 c("title", "slug", "topic", "state", "short_title", "poll_count", 
+                   "last_updated", "url", "estimates", "estimates_by_date"))
+    expect_equal(unname(sapply(chart, class)),
+                 list("character", "character", "character", "character", "character", 
+                      "numeric", c("POSIXct", "POSIXt"), "character", "data.frame", 
+                      "data.frame"))
+})
+
+test_that("chart$esimates is in expected format", {
+    expect_equal(names(chart$estimates),
+                 c("choice", "value", "lead_confidence", "first_name", "last_name", 
+                   "party", "incumbent"))
+    expect_equal(unname(sapply(chart$estimates, class)),
+                 c("factor", "numeric", "logical", "logical", "logical", "logical", 
+                   "logical"))
+})
+
+test_that("chart$esimates is in expected format", {
+    expect_equal(names(chart$estimates_by_date),
+                 c("choice", "value", "date"))
+    expect_equal(unname(sapply(chart$estimates_by_date, class)),
+                 c("factor", "numeric", "Date"))
+
+})
---
+++ tests/testthat/test-polls.R
@@ -0,0 +1,46 @@
+context("polls")
+
+pollsu <- pollster_polls(max_pages = 1, convert = FALSE)
+
+test_that("polls is in expected format if not converted", {
+    expect_is(pollsu, "list")
+    expect_true(length(pollsu) > 0)
+    expect_equal(names(pollsu[[1]]),
+                 c("id", "pollster", "start_date", "end_date", "method", "source", 
+                   "last_updated", "survey_houses", "sponsors", "questions"))
+})
+
+polls <- pollster_polls(max_pages = 1)
+
+test_that("polls data is in the correct format", {
+    expect_is(polls, "list")
+    expect_equal(length(polls), 2)
+    expect_equal(names(polls), c("polls", "questions"))
+    expect_equal(unname(sapply(polls, class)), rep("data.frame", 2))
+})
+
+test_that("query returns data", {
+    expect_true(nrow(polls[["polls"]]) > 0)
+    expect_true(nrow(polls[["questions"]]) > 0)
+})
+
+test_that("polls$polls is in expected format", {
+    expect_equal(names(polls$polls),
+                c("id", "pollster", "start_date", "end_date", "method", "source", 
+                  "last_updated", "survey_houses", "sponsors"))
+    expect_equal(unname(sapply(polls$polls, class)),
+                list("numeric", "factor", "Date", "Date", "factor", "factor", 
+                     c("POSIXct", "POSIXt"), "character", "character"))
+})
+    
+test_that("polls$polls is in expected format", {
+    expect_equal(names(polls$questions),
+                c("question", "chart", "topic", "state", "subpopulation", "observations", 
+                  "margin_of_error", "choice", "value", "first_name", "last_name", 
+                  "party", "incumbent", "id"))
+    expect_equal(unname(sapply(polls$questions, class)),
+                c("character", "character", "character", "character", "character", 
+                  "numeric", "numeric", "character", "numeric", "character", "character", 
+                  "character", "logical", "numeric"))
+})
+
