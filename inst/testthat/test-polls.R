785fc00afeed0fdde31c988c81d2f6ed75c236d0
jrnold <jeffrey.arnold@gmail.com>
2014-08-17 21:27:51 -0700

    removed testthat tests from package tests

--- inst/testthat/test-polls.R
+++ inst/testthat/test-polls.R
@@ -11,9 +11,9 @@ polls <- pollstr_polls(max_pages = 1)
 
 test_that("polls data is in the correct format", {
     expect_is(polls, "pollstr_polls")
-    expect_equal(length(polls), 2)
-    expect_equal(names(polls), c("polls", "questions"))
-    expect_equal(unname(sapply(polls, class)), rep("data.frame", 2))
+    expect_equal(length(polls), 4)
+    expect_equal(names(polls), c("polls", "questions", "survey_houses", "sponsors"))
+    expect_equal(unname(sapply(polls, class)), rep("data.frame", 4))
 })
 
 test_that("query returns data", {
