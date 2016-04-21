7c98012c910d1e49f7705e8607fb4fb351ccaa21
jrnold <jeffrey.arnold@gmail.com>
2016-04-20 20:40:25 -0700

    add pages arg to pollster_charts
    The Pollster API added a pages argument for the charts method.
    
    Fixes #10

--- R/charts.R
+++ R/charts.R
@@ -2,21 +2,24 @@
 NULL
 
 # Create URL for the charts API method
-pollstr_charts_url <- function(topic, state, showall) {
+pollstr_charts_url <- function(page, topic, state, showall) {
   query <- list()
-  if (! is.null(topic)) {
+  if (!is.null(page)) {
+    query[["page"]] <- as.character(page)[1]
+  }  
+  if (!is.null(topic)) {
     query[["topic"]] <- as.character(topic)[1]
   }
-  if (! is.null(state)) {
+  if (!is.null(state)) {
     query[["state"]] <- as.character(state)[1]
   }
-  if (! is.null(showall)) {
+  if (!is.null(showall)) {
     query[["showall"]] <- if (showall) "true" else "false"
   }
-  if (! length(query)) {
+  if (!length(query)) {
     query <- NULL
   }
-  modify_url(paste(.POLLSTR_API_URL, "charts", sep="/"), query = query)
+  modify_url(paste(.POLLSTR_API_URL, "charts", sep = "/"), query = query)
 }
 
 # clean up the objects returned by the API
@@ -47,13 +50,20 @@ charts2df <- function(.data) {
             class = c("pollstr_charts"))
 }
 
+get_charts_page <- function(page, topic, state, showall, as = "parsed") {
+  url <- pollstr_charts_url(page, topic, state, showall)
+  get_url(url, as = as)
+}
+
 #' Get list of available charts
 #'
+#' @param page Return page number
 #' @param state Only include charts from a single state. Use 2-letter state abbreviations. "US" will return all national charts.
 #' @param topic Only include charts related to a specific topic. See \url{http://elections.huffingtonpost.com/pollster/api} for examples.
 #' @param showall logical Include charts for races that were once possible but didn't happen (e.g. Gingrich vs. Obama 2012)
 #' @param convert Rearrange the data returned by the API into easier to use data frames.
-#'
+#' @param max_pages Maximum number of pages to get.
+#' 
 #' @references \url{http://elections.huffingtonpost.com/pollster/api}
 #' @return If \code{convert=TRUE}, a \code{"pollstr_charts"} object with elements
 #' \describe{
@@ -73,9 +83,20 @@ charts2df <- function(.data) {
 #'  allcharts <- pollstr_charts()
 #' }
 #' @export
-pollstr_charts <- function(topic = NULL, state = NULL, showall = NULL,
-                           convert = TRUE) {
-  .data <- get_url(pollstr_charts_url(topic, state, showall), as = "parsed")
+pollstr_charts <- function(page = 1, topic = NULL, state = NULL, showall = NULL,
+                           convert = TRUE, max_pages = 1) {
+  
+  .data <- list()
+  i <- 0L
+  while (i < max_pages) {
+    newdata <- get_charts_page(page + i, topic, state, showall)
+    if (length(newdata)) {
+      .data <- append(.data, newdata)
+    } else {
+      break
+    }
+    i <- i + 1L
+  }
   if (convert) .data <- charts2df(.data)
   .data
 }
