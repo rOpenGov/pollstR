f21bf21951aa6dfdd16450daecb7a3a3595a9655
jrnold <jeffrey.arnold@gmail.com>
2016-07-24 11:01:38 -0700

    remove temporary files
    I had moved them out of testthat while debugging the problem with failed tests. I accidentally committed them.

--- test-pollstr_chart_tbl.R
+++
@@ -1,7 +0,0 @@
-context("pollstr_chart_tbl")
-
-test_that("pollstr_chart_tbl works", {
-  skip_on_cran()
-  chart <- pollstr_chart_tbl("2016-general-election-trump-vs-clinton")
-  expect_is(chart, "data.frame")
-})
